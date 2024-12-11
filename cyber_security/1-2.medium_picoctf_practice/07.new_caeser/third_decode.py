a799d4f35668f922190ab37841e23c75a1421f71
Jin1 <jin1@iMac-von-TJ.local>
2024-12-11 11:51:47 +0100

    07.new_caeser:added

---
+++ cyber_security/1-2.medium_picoctf_practice/07.new_caeser/third_decode.py
@@ -0,0 +1,62 @@
+import string
+
+LOW_OFFSET = ord("a")
+ALPHA = string.ascii_lowercase[:16]
+
+def		b16_encode(input):
+	enc = ""
+	for c in input:
+		binary = "{0:08b}".format(ord(c))
+		enc += ALPHA[int(binary[:4], 2)]
+		enc += ALPHA[int(binary[4:], 2)]
+	return enc
+
+def		shift(str, k):
+	res = ""
+	idx_k = ord(k) - LOW_OFFSET
+	for c in str:
+		idx_c = ord(c) - LOW_OFFSET
+		res += ALPHA[(idx_c + idx_k) % len(ALPHA)]
+	return res
+
+#to test : properly encoded
+print(shift(b16_encode("a"), "p"))
+
+
+
+#We found a brand new type of encryption, 
+# can you break the secret code? 
+#(Wrap with picoCTF{}) 
+encoded = "dcebcmebecamcmanaedbacdaanafagapdaaoabaaafdbapdpaaapadanandcafaadbdaapdpandcac"
+print(f"encoded: {encoded}")
+
+#to decode
+def		rev_shift(str, k):
+	res = ""
+	idx_k = ord(k) - LOW_OFFSET
+	for c in str:
+		idx_c = ord(c) - LOW_OFFSET
+		res += ALPHA[(idx_c - idx_k) % len(ALPHA)]
+	return res
+
+key = "e"
+rev_shifted = rev_shift(encoded, key)
+print(f"rev_shifted with the key \"{key}\": \n{rev_shifted}")
+
+def		rev_b16_encode(input):
+	res = ""
+	print("rev: ")
+	for i in range(0, len(input), 2):
+		fi = ALPHA.index(input[i])
+		se = ALPHA.index(input[i + 1])
+		ch = "{0:04b}{1:04b}".format(fi, se)
+		n = int(ch, 2)
+		print(f"{{{ch} -> {n}}}")
+		#print(n, end="")
+		res += chr(n)
+	return res
+		# ch_to_int = int(ch, 2)
+		# res += ALPHA[ch_to_int]
+
+rev_encoded = rev_b16_encode(rev_shifted)
+print(f"(in: \"{rev_shifted}\") \n -> out: \"{rev_encoded}\" ")
